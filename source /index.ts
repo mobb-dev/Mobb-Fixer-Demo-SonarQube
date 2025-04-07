add8bfbece12bf7932abeeda94170601a1ba6018
Antony Chiu <antony@mobb.ai>
2025-04-07 16:55:32 -0600

    Create index.ts

---
+++ source /index.ts
@@ -0,0 +1,28 @@
+/*
+    MOBB.dev unsafe code example for DOM XSS 
+*/
+
+import {testFunc} from "./stringSanitize";
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const unsafe_var1 = urlParams.get('var1');
+const unsafe_var2 = urlParams.get('var2');
+const unsafe_div = window.document.getElementById("vulnerableDiv")!;
+
+if (unsafe_var1){
+    const unsafe_html1 = '<b>' + unsafe_var1 + '</b>';
+    document.write(unsafe_var1)
+    document.writeln('<b>' + unsafe_var1 + '</b>')
+    document.write(testFunc(unsafe_html1))
+    
+}
+if (unsafe_var2){
+    if (unsafe_div){
+        const unsafe_html2 = '<h1>' + unsafe_var2 + '</h1>';
+        unsafe_div.innerHTML = unsafe_var2;
+        unsafe_div.outerHTML = unsafe_html2;
+        unsafe_div.innerHTML = '<h1>' + unsafe_var2 + '</h1>';
+        unsafe_div.innerHTML = testFunc(unsafe_var2);
+    }
+}
